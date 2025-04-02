7dade94f7aeefeabc8c81b80d3d35f7e5b1de4ca
Amar Kulkarni <ark8su@virginia.edu>
2025-04-02 19:26:17 -0400

    fix build issues

--- src/take_home_node/src/take_home.cpp
+++ src/take_home_node/src/take_home.cpp
@@ -1,8 +1,10 @@
 #include "take_home_node/take_home.hpp"
+#include <rclcpp_components/register_node_macro.hpp>
 
 TakeHome::TakeHome(const rclcpp::NodeOptions& options)
     : Node("take_home_metrics", options) {
 
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
     odometry_subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>(
       "vehicle/uva_odometry", qos_profile,
       std::bind(&TakeHome::odometry_callback, this, std::placeholders::_1));
@@ -15,9 +17,10 @@ void TakeHome::odometry_callback(nav_msgs::msg::Odometry::ConstSharedPtr odom_ms
   float position_y = odom_msg->pose.pose.position.y;
   float position_z = odom_msg->pose.pose.position.z;
 
-  // Do stuff with this callback!
+  // Do stuff with this callback! or more, idc
   std_msgs::msg::Float32 metric_msg;
-  metric_publisher_->publish(metric_msg)
+  metric_msg.data = (position_x + position_y + position_z) / (position_x + position_z); // Example metric calculation
+  metric_publisher_->publish(metric_msg);
 }
 
 
